72dbcf9f58a61ea6d0520115ad460ae21ed395e8
keyhansa <keyhan.ahmad@gmail.com>
2025-09-20 20:34:45 +0330

    no rounded buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,21 +3,21 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-    "inline-flex flex-row-reverse gap-2 items-center justify-center rounded-md text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+    "inline-flex flex-row-reverse gap-2 items-center justify-center rounded-[0.125rem] text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
     {
       variants: {
         variant: {
           default: "bg-primary text-primary-foreground hover:bg-primary/90",
           destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-          outline: "border border-input hover:bg-accent hover:text-accent-foreground",
+          outline: "border-2 border-input hover:bg-accent hover:text-accent-foreground",
           secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
           ghost: "hover:bg-accent hover:text-accent-foreground", // Ghost has no background until hovered
           link: "underline-offset-4 hover:underline text-primary",
         },
         size: {
           default: "h-10 py-2 px-4",
-          sm: "h-9 px-3 rounded-md",
-          lg: "h-11 px-8 rounded-md",
+          sm: "h-9 px-3",
+          lg: "h-11 px-8",
           icon: "h-10 w-10 p-0", // Icon size is square with no padding
         },
       },
@@ -39,7 +39,7 @@ const buttonVariants = cva(
       // Use the appropriate element based on asChild prop
       const Comp = asChild ? React.Fragment : "button"
   
-      return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+      return <button className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
     },
   )
   Button.displayName = "Button"
