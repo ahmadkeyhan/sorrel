1f2e217ea1cb04db4f33029bf741d21b4893a62e
keyhansa <keyhan.ahmad@gmail.com>
2025-09-20 21:42:06 +0330

    hovers ommited

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,11 +7,11 @@ const buttonVariants = cva(
     {
       variants: {
         variant: {
-          default: "bg-primary text-primary-foreground hover:bg-primary/90",
-          destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-          outline: "border-2 border-input hover:bg-accent hover:text-accent-foreground",
-          secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-          ghost: "hover:bg-accent hover:text-accent-foreground", // Ghost has no background until hovered
+          default: "bg-primary text-primary-foreground",
+          destructive: "bg-destructive text-destructive-foreground",
+          outline: "border-2 border-input",
+          secondary: "bg-secondary text-secondary-foreground",
+          ghost: "", // Ghost has no background until hovered
           link: "underline-offset-4 hover:underline text-primary",
         },
         size: {
